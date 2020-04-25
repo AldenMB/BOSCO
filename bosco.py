344b1e5a5d12e13d40923aeb0969e89186c9c64e
Alden Bradford <aldenbradford@gmail.com>
2020-04-24 22:25:32 -0400

    added some idle activities

--- bosco.py
+++ bosco.py
@@ -4,6 +4,7 @@ from gpiozero import AngularServo
 from neopixel import NeoPixel
 from board import D18
 import time
+import random
 
 left_arm = None
 right_arm = None
@@ -41,8 +42,22 @@ def sting():
     p.start()
     return p
 
-def espeak(str, args = ['-ven-us']):
-    p = Process(target = run, args = [['espeak-ng']+args+[str]])
+def espeak(str,
+        wpm = 175, 
+        voice = 'en-us', 
+        pitch = 50,
+        amplitude = 100,
+        whisper = False, 
+        croak = False):
+    if whisper:
+        voice += '+whisper'
+    if croak:
+        voice += '+croak'
+    command = f'espeak-ng -v{voice} -s{wpm} -p{pitch} -a{amplitude}'
+    return run(command.split()+[str])
+
+def say(str):
+    p = Process(target = espeak, kwargs = {'wpm':150})
     p.start()
     return p
 
@@ -50,7 +65,7 @@ def blink(duration):
     def offon():
         lights[6:] = [(0,0,0)]*2
         time.sleep(duration)
-        lights[6:] = [(0,0,0)]*2
+        lights[6:] = [(255,255,255)]*2
     p = Process(target = offon)
     p.start()
     return p
@@ -87,6 +102,35 @@ servo_paths = {
     'quadratic': (lambda x: x**2/2 if x <= 0.5 else 1-(1-x)**2/2),
     'cubic': (lambda x: (3 - 2*x) * x**2),
 }
+
+def fidget():
+    def go():
+        while True:
+            servo = random.choice([left_arm, right_arm, head])
+            movetime = random.uniform(2,4)
+            moveplace = random.triangular(-1,1,0)
+            waittime = random.uniform(5,10)
+            m = move(servo, moveplace, movetime, path='quadratic')
+            m.join()
+            time.sleep(waittime)
+    p = Process(target = go)
+    p.start()
+    return p
+
+def cylon(color, period = 1/6):
+    def go():
+        direction = 1
+        position = 0
+        while True:
+            lights[position] = 0
+            position += direction
+            lights[position] = color
+            if position in [0,5]:
+                direction = -direction
+            time.sleep(period)
+    p = Process(target = go)
+    p.start()
+    return p
 
 if __name__ == '__main__':
     init()
